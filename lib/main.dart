8894482add31843ac4f580b4806b9a13094da443
rama <ramaaldurra@gmail.com>
2025-07-14 13:55:54 +0300

    new3

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen()
+      home: LoginScreen()
       // PropertyDetailsPage(
       //   images: [
       //     'images/Living_Room.jpg',
